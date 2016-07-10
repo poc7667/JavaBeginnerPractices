9027e9a8fb57f090b45153074b567ed83ee7d377
poc <poc7667@gmail.com>
2016-07-10 10:53:06 -0700

    add hw4

--- hw4/src/com/poc/DrawLine_4_4.java
+++ hw4/src/com/poc/DrawLine_4_4.java
@@ -3,14 +3,12 @@ package com.poc;
 /**
  * Created by poc on 7/10/16.
  */
-public class DrawLine_4_2 {
-
+public class DrawLine_4_4 {
     public static void main(String[] args) {
         System.out.println("Using Brutal force:");
 
         System.out.println("---------------------");
-        int i = 0;
-        while (i++ < 8){
+        for(int i=0;i<8;i++){
             System.out.printf("|%20s\n","|");
         }
         System.out.println("---------------------");
